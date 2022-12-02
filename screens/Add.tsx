bf324b8371c8f3bfdabcb17220c00fd6309301a5
Lazar Nikolov <lazar.nikolov@sentry.io>
2022-12-02 21:02:21 +0100

    add screen form complete

--- screens/Add.tsx
+++ screens/Add.tsx
@@ -7,25 +7,63 @@ import {
   TouchableHighlight,
   InputAccessoryView,
   Keyboard,
+  Platform,
 } from 'react-native';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import DateTimePicker, {
+  DateTimePickerAndroid,
+} from '@react-native-community/datetimepicker';
 
 import { ListItem } from '../components/ListItem';
 import { Recurrence } from '../types/recurrence';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import BottomSheet, { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import { theme } from '../theme';
+import { Category } from '../types/category';
+
+const CATEGORIES: Category[] = [
+  {
+    id: '1',
+    name: 'Food',
+    color: '#FFD600',
+  },
+  {
+    id: '2',
+    name: 'Transport',
+    color: '#FF6D00',
+  },
+  {
+    id: '3',
+    name: 'Entertainment',
+    color: '#00C853',
+  },
+  {
+    id: '4',
+    name: 'Shopping',
+    color: '#2962FF',
+  },
+];
 
 export const Add = () => {
   const sheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['25%', '50%', '90%'], []);
 
+  const [sheetView, setSheetView] = React.useState<'recurrence' | 'category'>(
+    'recurrence'
+  );
   const [amount, setAmount] = React.useState('');
   const [recurrence, setRecurrence] = React.useState<string>(Recurrence.None);
-  const [category, setCategory] = React.useState('');
+  const [date, setDate] = React.useState(new Date());
+  const [note, setNote] = React.useState('');
+  const [category, setCategory] = React.useState<Category>(CATEGORIES[0]);
 
   const selectRecurrence = (selectedRecurrence: string) => {
     setRecurrence(selectedRecurrence as Recurrence);
+    sheetRef.current?.close();
+  };
+
+  const selectCategory = (selectedCategory: Category) => {
+    setCategory(selectedCategory);
     sheetRef.current?.close();
   };
 
@@ -74,6 +112,7 @@ export const Add = () => {
                   alignItems: 'center',
                 }}
                 onPress={() => {
+                  setSheetView('recurrence');
                   sheetRef.current?.snapToIndex(1);
                 }}
               >
@@ -85,6 +124,75 @@ export const Add = () => {
                   }}
                 >
                   {recurrence}
+                </Text>
+              </TouchableOpacity>
+            }
+          />
+          <ListItem
+            label='Date'
+            detail={
+              Platform.OS === 'ios' && (
+                <DateTimePicker
+                  value={date}
+                  mode={'date'}
+                  is24Hour={true}
+                  themeVariant='dark'
+                  maximumDate={new Date()}
+                  minimumDate={
+                    new Date(
+                      new Date().getFullYear() - 1,
+                      new Date().getMonth(),
+                      new Date().getDate()
+                    )
+                  }
+                  onChange={(event, newDate) => setDate(newDate)}
+                />
+              )
+            }
+          />
+          <ListItem
+            label='Note'
+            detail={
+              <TextInput
+                placeholder='Note'
+                onChange={(event) => setNote(event.nativeEvent.text)}
+                value={note}
+                textAlign='right'
+                inputAccessoryViewID='dismissKeyboard'
+                style={{
+                  height: 40,
+                  color: 'white',
+                  flex: 1,
+                  borderRadius: 8,
+                  paddingLeft: 8,
+                  fontSize: 16,
+                }}
+              />
+            }
+          />
+          <ListItem
+            label='Category'
+            detail={
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  setSheetView('category');
+                  sheetRef.current?.snapToIndex(1);
+                }}
+              >
+                <Text
+                  style={{
+                    color: category.color,
+                    textTransform: 'capitalize',
+                    fontSize: 16,
+                  }}
+                >
+                  {category.name}
                 </Text>
               </TouchableOpacity>
             }
@@ -103,19 +211,58 @@ export const Add = () => {
         enablePanDownToClose
         snapPoints={snapPoints}
       >
-        <BottomSheetFlatList
-          data={Object.keys(Recurrence)}
-          keyExtractor={(i) => i}
-          renderItem={(item) => (
-            <TouchableHighlight
-              style={{ paddingHorizontal: 18, paddingVertical: 12 }}
-              onPress={() => selectRecurrence(item.item)}
-            >
-              <Text style={{ color: 'white', fontSize: 18 }}>{item.item}</Text>
-            </TouchableHighlight>
-          )}
-          style={{ backgroundColor: theme.colors.card }}
-        />
+        {sheetView === 'recurrence' && (
+          <BottomSheetFlatList
+            data={Object.keys(Recurrence)}
+            keyExtractor={(i) => i}
+            renderItem={(item) => (
+              <TouchableHighlight
+                style={{ paddingHorizontal: 18, paddingVertical: 12 }}
+                onPress={() => selectRecurrence(item.item)}
+              >
+                <Text style={{ color: 'white', fontSize: 18 }}>
+                  {item.item}
+                </Text>
+              </TouchableHighlight>
+            )}
+            style={{ backgroundColor: theme.colors.card }}
+          />
+        )}
+        {sheetView === 'category' && (
+          <BottomSheetFlatList
+            data={CATEGORIES}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item }) => (
+              <TouchableHighlight
+                style={{ paddingHorizontal: 18, paddingVertical: 12 }}
+                onPress={() => selectCategory(item)}
+              >
+                <View
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}
+                >
+                  <View
+                    style={{
+                      backgroundColor: item.color,
+                      width: 12,
+                      height: 12,
+                      borderRadius: 6,
+                    }}
+                  />
+                  <Text
+                    style={{ color: 'white', fontSize: 18, marginLeft: 12 }}
+                  >
+                    {item.name}
+                  </Text>
+                </View>
+              </TouchableHighlight>
+            )}
+            style={{ backgroundColor: theme.colors.card }}
+          />
+        )}
       </BottomSheet>
       <InputAccessoryView nativeID='dismissKeyboard'>
         <View
